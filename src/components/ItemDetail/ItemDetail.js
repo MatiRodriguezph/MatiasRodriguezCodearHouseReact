97f81facef0f5c2f72e1cb3eb3844f87ecef21ba
Mati.Rodriguez <Mati.rodriguezph@gmail.com>
2021-10-11 18:54:24 -0300

    Entrega nro 11 react

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -6,23 +6,22 @@ const ItemDetail = (props) => {
 
     const carrito = useContext(contextoCarrito)
     
-    let {id, name, categoria, description, price, stock, pictureUrl, cantidadComprada} = props.producto
+    let {id, title, categoryId, description, price, stock, pictureUrl, cantidadComprada} = props.producto
 
     const agregarAlCarrito = (cantidadAgregadaAlCarrito) => {    
         cantidadComprada = cantidadAgregadaAlCarrito
-        carrito.addItem({id, name, categoria, description, price, stock, pictureUrl, cantidadComprada}, cantidadAgregadaAlCarrito)
+        carrito.addItem({id, title, categoryId, description, price, stock, pictureUrl, cantidadComprada}, cantidadAgregadaAlCarrito)
     }
 
     return (
         <div id="itemDetailDiv">
-            <h1>{name}</h1>
-            <h6>{categoria}</h6>
+            <h1>{title}</h1>
             <hr/>
             <img src={pictureUrl}></img>
             <p>{description}</p>
             <h3>Precio: $ {price}</h3>
             <h4>Unidades disponibles: {stock}</h4>
-            <ItemCount id ={id} stock={10} initial={1} onAdd={agregarAlCarrito}/>
+            <ItemCount id ={id} stock={stock} initial={1} onAdd={agregarAlCarrito}/>
         </div>
     )
 }
--- src/components/ItemList/Item.js
+++ src/components/ItemList/Item.js
@@ -2,22 +2,31 @@ import Card from "react-bootstrap/Card"
 import Button from "react-bootstrap/Button"
 import ItemCount from "../ItemCount/ItemCount"
 import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { contextoCarrito } from "../Context/CartContext"
 
 const Item = (props) => {
 
-    const {id, name, categoria, description, price, stock, pictureUrl} = props.item
+    let {id, title, categoryId, description, price, stock, pictureUrl, cantidadComprada} = props.item
+    
+    const carrito = useContext(contextoCarrito)
+
+    const agregarAlCarrito = (cantidad) => {
+        cantidadComprada = cantidad
+        carrito.addItem({id, title, categoryId, description, price, stock, pictureUrl, cantidadComprada}, cantidad)
+    }
 
     return (
         <Card className="m-3 box-shadow" style={{ width: '18rem' }}>
             <Card.Img variant="top" src={pictureUrl} style={{ width: '200px', height: "auto" }} />
             <Card.Body>
-                <Link item={props.item} to={`/item/${id}`}><Card.Title>{name}</Card.Title></Link>
+                <Link item={props.item} to={`/item/${id}`}><Card.Title>{title}</Card.Title></Link>
                 <Card.Text>
                     <span>Precio: $ {price}</span><br/>
                     <span>Stock disponible: {stock}</span>
                 </Card.Text>
                 <div class="d-flex align-items-baseline">
-                    <ItemCount stock={5} initial={1} onAdd={() => {}}/>
+                    <ItemCount stock={stock} initial={1} onAdd={(cantidad) => agregarAlCarrito(cantidad)}/>
                 </div>
             </Card.Body>
         </Card>

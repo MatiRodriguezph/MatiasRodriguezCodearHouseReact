0e126e88a5ace0c31cbd0f2d61195e602c07e6f4
Mati.Rodriguez <Mati.rodriguezph@gmail.com>
2021-10-13 22:11:53 -0300

    entrega firebase

--- src/components/ItemList/ItemListContainer.js
+++ src/components/ItemList/ItemListContainer.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import ItemList from "./ItemList";
 import { arrayProductos } from "../../datos";
 import Spinner from 'react-bootstrap/Spinner';
+import { firestore } from "../../firebase";
 
 const ItemListContainer = () => {
     
@@ -11,30 +12,34 @@ const ItemListContainer = () => {
 
     useEffect(() => {
 
-        const promesa = new Promise((resolve, reject) => {
-            document.getElementById("spinner").style.display = "block"
-            setTimeout(() => {
-                resolve(arrayProductos)
+        const db = firestore
+
+
+        const coleccion = db.collection("productos")
+        
+        let consulta
+        if (!parametros.id) consulta = coleccion.get()
+        if (parametros.id == 1) consulta = coleccion.where("categoryId", "==", "1").get()
+        if (parametros.id == 2) consulta = coleccion.where("categoryId", "==", "2").get()
+        if (parametros.id == 3) consulta = coleccion.where("categoryId", "==", "3").get()
+
+        consulta
+            .then(res => {
+                const documento = res.docs
+                let auxiliarProductos = []
+
+                documento.forEach(producto => {
+                    const consultaFinal = {
+                        id: producto.id,
+                        ...producto.data()
+                    }
+                    auxiliarProductos.push(consultaFinal)
+                })
+
+                setProductos(auxiliarProductos)
                 document.getElementById("spinner").style.display = "none"
-            }, 2000)
-        })
-
-        promesa.then(res => {
-            if (parametros.id == 1){
-                res = res.filter(a => a.categoria == "Notebooks");
-            }
-
-            if (parametros.id == 2){
-                res = res.filter(a => a.categoria == "Perifericos");
-            }
-
-            if (parametros.id == 3){
-                res = res.filter(a => a.categoria == "Sillas gamer");
-            }
-
-            setProductos(res)
-        })
-
+            })
+            .catch(err => console.log(err))
     }, [parametros])
     
     return (    
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+//Traer firebase
+import firebase from "firebase/app"
+
+//Traer el servicio de firestore
+import "firebase/firestore";
+
+
+//Inicializar el servicio
+const firebaseConfig = {
+apiKey: "AIzaSyDbOKnQIZsLCwm_YQAYrnTLxkRqoN_0xsk",
+authDomain: "coderhousee-commercematiasr.firebaseapp.com",
+projectId: "coderhousee-commercematiasr",
+storageBucket: "coderhousee-commercematiasr.appspot.com",
+messagingSenderId: "746557105197",
+appId: "1:746557105197:web:b93c7ca64ff7cb197a44a4"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+//Tener la referencia de la base de datos para importarla en el resto de la app
+export const firestore = firebase.firestore(app)
